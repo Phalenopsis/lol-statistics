22b7e6a00df8bd5b2edee581acc17388107e0786
Nicolas Leroux <hertford.fr@gmail.com>
2025-05-01 18:12:31 +0200

    refactor: use HashMap on fly rather than stock in arrayList first

--- src/main/java/org/aaron/domain/team/Team.java
+++ src/main/java/org/aaron/domain/team/Team.java
@@ -3,17 +3,14 @@ package org.aaron.domain.team;
 import org.aaron.domain.match.MatchResult;
 import org.aaron.domain.match.Result;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class Team implements Comparable<Team>{
     private final Series series;
     private final Games games;
     private final String name;
-    private final Map<String, Result> matchResultMap = new HashMap<>();
-    private List<MatchResult> matchResultList = new ArrayList<>();
+    private Map<String, Result> versusMatchResults = new HashMap<>();
 
     public Team(String name) {
         this.name = name;
@@ -21,20 +18,18 @@ public class Team implements Comparable<Team>{
         this.games = new Games();
     }
 
-    public Team(String name, Series series, Games games, List<MatchResult> matchResultList) {
+    public Team(String name, Series series, Games games, Map<String, Result> versusMatchResults) {
         this.series = series;
         this.games = games;
         this.name = name;
-        this.matchResultList = matchResultList;
-        prepareMatchResults();
+        this.versusMatchResults.putAll(versusMatchResults);
     }
 
     public Team(Team team) {
         name = team.name;
         games = new Games(team.getGames());
         series = new Series(team.getSeries());
-        matchResultList = List.copyOf(team.matchResultList);
-        prepareMatchResults();
+        versusMatchResults.putAll(team.versusMatchResults);
     }
 
     public String getName() {
@@ -57,22 +52,16 @@ public class Team implements Comparable<Team>{
         return games;
     }
 
-    private void prepareMatchResults() {
-        for (MatchResult matchResult: matchResultList) {
-            matchResultMap.put(matchResult.oppositeTeam(), matchResult.result());
-        }
-    }
 
     public Team computeScore(String opponentName, int victory, int defeat) {
         MatchResult matchResult = new MatchResult(opponentName, victory == 2 ? Result.VICTORY : Result.DEFEAT);
-        ArrayList<MatchResult> list = new ArrayList<>(matchResultList);
-        list.add(matchResult);
+        versusMatchResults.put(matchResult.oppositeTeam(), matchResult.result());
 
         return new Team(
                 name,
                 new Series(series.getVictory() + (victory == 2 ? 1 : 0), series.getDefeat() + (victory == 2 ? 0 : 1)),
                 new Games(games.getVictory() + victory, games.getDefeat() + defeat),
-                list
+                versusMatchResults
         );
     }
 
@@ -91,8 +80,8 @@ public class Team implements Comparable<Team>{
         if(seriesComparison == 0) {
             int gamesComparison = games.compareTo(o.games);
             if(gamesComparison == 0) {
-                if(matchResultMap.containsKey(o.getName())) {
-                    Result result = matchResultMap.get(o.getName());
+                if(versusMatchResults.containsKey(o.getName())) {
+                    Result result = versusMatchResults.get(o.getName());
                     if(result.equals(Result.VICTORY)) return -1;
                     return 1;
                 }

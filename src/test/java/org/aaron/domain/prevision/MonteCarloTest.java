0d2d6c243c51487dc4f4306b1b1220950836fded
Nicolas Leroux <hertford.fr@gmail.com>
2025-05-01 21:58:57 +0200

    test: add tests for monte carlo

---
+++ src/test/java/org/aaron/domain/prevision/MonteCarloTest.java
@@ -0,0 +1,139 @@
+package org.aaron.domain.prevision;
+
+import org.aaron.domain.match.Match;
+import org.aaron.domain.ranking.Championship;
+import org.aaron.domain.ranking.StatisticsForGivenRank;
+import org.aaron.domain.team.Team;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MonteCarloTest {
+    String znt = "ZennIt";
+    String mcon = "mCon esports";
+    String snsh = "Senshi eports";
+    String aoma = "A One Man Army";
+    String myth = "Myth Esport";
+    String ouat = "Once Upon A Team";
+    String jmgg = "Jörmungang";
+    String ara = "Aurora";
+
+    @Test
+    void testCompute_With2Teams() {
+        Team mconTeam = new Team(mcon);
+        Team snshTeam = new Team(snsh);
+
+        List<Team> teamList = List.of(mconTeam, snshTeam);
+        Match match = new Match(mcon, snsh);
+        ArrayList<Match> matches = new ArrayList<>(List.of(match));
+        Championship championship = new Championship(teamList, matches);
+
+        championship.computeMonteCarlo();
+
+        Map<Integer, StatisticsForGivenRank> result = championship.getStatisticsForRanks();
+
+        StatisticsForGivenRank rank1 = result.get(1);
+        double chancesForMconToFinish1 = rank1.getChances(mcon);
+        double chancesForSnshToFinish1 = rank1.getChances(snsh);
+        StatisticsForGivenRank rank2 = result.get(2);
+        double chancesForMconToFinish2 = rank2.getChances(mcon);
+        double chancesForSnshToFinish2 = rank2.getChances(snsh);
+
+        Assertions.assertTrue(chancesForMconToFinish1 >= 49 && chancesForMconToFinish1 <= 51);
+        Assertions.assertTrue(chancesForSnshToFinish1 >= 49 && chancesForSnshToFinish1 <= 51);
+
+        Assertions.assertTrue(chancesForMconToFinish2 >= 49 && chancesForMconToFinish2 <= 51);
+        Assertions.assertTrue(chancesForSnshToFinish2 >= 49 && chancesForSnshToFinish2 <= 51);
+    }
+
+    @Test
+    void testCompute_With8Teams() {
+        Team zntTeam = new Team(znt);
+        Team mconTeam = new Team(mcon);
+        Team snshTeam = new Team(snsh);
+        Team aomaTeam = new Team(aoma);
+        Team mythTeam = new Team(myth);
+        Team ouatTeam = new Team(ouat);
+        Team jmggTeam = new Team(jmgg);
+        Team araTeam = new Team(ara);
+
+        List<Team> teams = List.of(zntTeam, mconTeam, snshTeam, aomaTeam, mythTeam, ouatTeam, jmggTeam, araTeam);
+
+        Match matchToDoW1_1 = new Match(myth, snsh);
+        Match matchToDoW1_2 = new Match(ouat, mcon);
+        Match matchToDoW1_3 = new Match(aoma, jmgg);
+        Match matchToDoW1_4 = new Match(znt, ara);
+
+        Match matchToDoW2_1 = new Match(znt, mcon);
+        Match matchToDoW2_2 = new Match(aoma,snsh);
+        Match matchToDoW2_3 = new Match(ouat, ara);
+        Match matchToDoW2_4 = new Match(myth, jmgg);
+
+        List<Match> matchList1 = List.of(matchToDoW1_1, matchToDoW1_2, matchToDoW1_3, matchToDoW1_4, matchToDoW2_1, matchToDoW2_2, matchToDoW2_3, matchToDoW2_4);
+
+        Match matchToDoW3_1 = new Match(myth, ara);
+        Match matchToDoW3_2 = new Match(ouat, jmgg);
+        Match matchToDoW3_3 = new Match(aoma, mcon);
+        Match matchToDoW3_4 = new Match(znt, snsh);
+
+        Match matchToDoW4_1 = new Match(znt, jmgg);
+        Match matchToDoW4_2 = new Match(myth, mcon);
+        Match matchToDoW4_3 = new Match(ouat, snsh);
+        Match matchToDoW4_4 = new Match(aoma, ara);
+
+        List<Match> matchList2 = List.of(matchToDoW3_1, matchToDoW3_2, matchToDoW3_3, matchToDoW3_4, matchToDoW4_1, matchToDoW4_2, matchToDoW4_3, matchToDoW4_4);
+
+        Match matchToDoW5_1 = new Match(mcon, jmgg);
+        Match matchToDoW5_2 = new Match(snsh, ara);
+        Match matchToDoW5_3 = new Match(znt, aoma);
+        Match matchToDoW5_4 = new Match(ouat, myth);
+
+        List<Match> matchList3 = List.of(matchToDoW5_1, matchToDoW5_2, matchToDoW5_3, matchToDoW5_4);
+
+        Match matchToDoW6_1 = new Match(mcon, ara);
+        Match matchToDoW6_2 = new Match(znt, ouat);
+        Match matchToDoW6_3 = new Match(snsh, jmgg);
+        Match matchToDoW6_4 = new Match(aoma, myth);
+
+        Match matchToDoW7_1 = new Match(ouat, aoma);
+        Match matchToDoW7_2 = new Match(ara, jmgg);
+        Match matchToDoW7_3 = new Match(znt, myth);
+        Match matchToDoW7_4 = new Match(mcon, snsh);
+
+
+        List<Match> matchList4 = List.of(matchToDoW6_1, matchToDoW6_2, matchToDoW6_3, matchToDoW6_4, matchToDoW7_1, matchToDoW7_2, matchToDoW7_3, matchToDoW7_4);
+
+        ArrayList<Match> matchList = new ArrayList<>(matchList1);
+        matchList.addAll(matchList2);
+        matchList.addAll(matchList3);
+        matchList.addAll(matchList4);
+
+        Championship championship = new Championship(teams, matchList);
+
+        championship.computeMonteCarlo();
+
+        Map<Integer, StatisticsForGivenRank> result = championship.getStatisticsForRanks();
+        StatisticsForGivenRank rank1 = result.get(1);
+        double chancesForMconToFinish1 = rank1.getChances(mcon);
+        double chancesForSnshToFinish1 = rank1.getChances(snsh);
+        StatisticsForGivenRank rank2 = result.get(2);
+        double chancesForMconToFinish2 = rank2.getChances(mcon);
+        double chancesForSnshToFinish2 = rank2.getChances(snsh);
+
+        double chances = 12.5;
+        double lowLimit = chances - 1;
+        double highLimit = chances + 1;
+
+        Assertions.assertTrue(chancesForMconToFinish1 >= lowLimit && chancesForMconToFinish1 <= highLimit);
+        Assertions.assertTrue(chancesForSnshToFinish1 >= lowLimit && chancesForSnshToFinish1 <= highLimit);
+
+        Assertions.assertTrue(chancesForMconToFinish2 >= lowLimit && chancesForMconToFinish2 <= highLimit);
+        Assertions.assertTrue(chancesForSnshToFinish2 >= lowLimit && chancesForSnshToFinish2 <= highLimit);
+
+    }
+}

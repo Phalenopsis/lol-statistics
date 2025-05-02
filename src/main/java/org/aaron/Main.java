43f7f0e5884e087159ca38312e5d74701210f6b0
Nicolas Leroux <hertford.fr@gmail.com>
2025-05-02 22:26:48 +0200

    refactor: refactor main and correct error in week 6

--- src/main/java/org/aaron/Main.java
+++ src/main/java/org/aaron/Main.java
@@ -3,6 +3,7 @@ package org.aaron;
 import org.aaron.domain.match.Match;
 import org.aaron.domain.match.Score;
 import org.aaron.domain.ranking.Championship;
+import org.aaron.domain.team.RankedTeam;
 import org.aaron.domain.team.Team;
 
 import java.util.ArrayList;
@@ -11,6 +12,15 @@ import java.util.List;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    private static String znt = "ZennIt";
+    private static String mcon = "mCon esports";
+    private static String snsh = "Senshi eports";
+    private static String aoma = "A One Man Army";
+    private static String myth = "Myth Esport";
+    private static String ouat = "Once Upon A Team";
+    private static String jmgg = "Jörmungang";
+    private static String ara = "Aurora";
+
     public static void main(String[] args) {
         Championship championship = getBeneluxChampionship();
 
@@ -26,6 +36,13 @@ public class Main {
 
         playWeek6(championship);
 
+        //playWeek7(championship);
+
+
+        List<RankedTeam> list = championship.getPlacedTeams();
+
+        System.out.println(list);
+
         long start = System.currentTimeMillis();
         championship.computeAllRemainingMatches();
         long finish = System.currentTimeMillis();
@@ -34,11 +51,45 @@ public class Main {
         System.out.println(championship.giveResults());
     }
 
+    private static List<Match> prepareWeek7() {
+        Match matchToDo1 = new Match(ouat, aoma);
+        Match matchToDo2 = new Match(ara, jmgg);
+        Match matchToDo3 = new Match(znt, myth);
+        Match matchToDo4 = new Match(mcon, snsh);
+
+        return List.of(matchToDo1, matchToDo2, matchToDo3, matchToDo4);
+    }
+
+    private static void playWeek7(Championship championship) {
+        championship.playNextMatch(new Score(0, 2));
+        championship.playNextMatch(new Score(0, 2));
+        //championship.playNextMatch(new Score(2, 1));
+        //championship.playNextMatch(new Score(2, 0));
+    }
+
+    private static List<Match> prepareWeek6() {
+        Match matchToDo1 = new Match(mcon, ara);
+        Match matchToDo2 = new Match(znt, ouat);
+        Match matchToDo3 = new Match(snsh, jmgg);
+        Match matchToDo4 = new Match(aoma, myth);
+
+        return List.of(matchToDo1, matchToDo2, matchToDo3, matchToDo4);
+    }
+
     private static void playWeek6(Championship championship) {
         championship.playNextMatch(new Score(2, 0));
-        championship.playNextMatch(new Score(2, 2));
+        championship.playNextMatch(new Score(2, 0));
         championship.playNextMatch(new Score(2, 1));
         championship.playNextMatch(new Score(2, 0));
+    }
+
+    private static List<Match> prepareWeek5() {
+        Match matchToDo1 = new Match(mcon, jmgg);
+        Match matchToDo2 = new Match(snsh, ara);
+        Match matchToDo3 = new Match(znt, aoma);
+        Match matchToDo4 = new Match(ouat, myth);
+
+        return List.of(matchToDo1, matchToDo2, matchToDo3, matchToDo4);
     }
 
     private static void playWeek5(Championship championship) {
@@ -48,11 +99,29 @@ public class Main {
         championship.playNextMatch(new Score(0, 2));
     }
 
+    private static List<Match> prepareWeek4() {
+        Match matchToDo1 = new Match(znt, jmgg);
+        Match matchToDo2 = new Match(myth, mcon);
+        Match matchToDo3 = new Match(ouat, snsh);
+        Match matchToDo4 = new Match(aoma, ara);
+
+        return List.of(matchToDo1, matchToDo2, matchToDo3, matchToDo4);
+    }
+
     private static void playWeek4(Championship championship) {
         championship.playNextMatch(new Score(2, 0));
         championship.playNextMatch(new Score(1, 2));
         championship.playNextMatch(new Score(1, 2));
         championship.playNextMatch(new Score(2, 0));
+    }
+
+    private static List<Match> prepareWeek3() {
+        Match matchToDo1 = new Match(myth, ara);
+        Match matchToDo2 = new Match(ouat, jmgg);
+        Match matchToDo3 = new Match(aoma, mcon);
+        Match matchToDo4 = new Match(znt, snsh);
+
+        return List.of(matchToDo1, matchToDo2, matchToDo3, matchToDo4);
     }
 
     private static void playWeek3(Championship championship) {
@@ -62,11 +131,29 @@ public class Main {
         championship.playNextMatch(new Score(2, 0));
     }
 
+    private static List<Match> prepareWeek2() {
+        Match matchToDo1 = new Match(znt, mcon);
+        Match matchToDo2 = new Match(aoma,snsh);
+        Match matchToDo3 = new Match(ouat, ara);
+        Match matchToDo4 = new Match(myth, jmgg);
+
+        return List.of(matchToDo1, matchToDo2, matchToDo3, matchToDo4);
+    }
+
     private static void playWeek2(Championship championship) {
         championship.playNextMatch(new Score(2, 1));
         championship.playNextMatch(new Score(1, 2));
         championship.playNextMatch(new Score(2, 1));
         championship.playNextMatch(new Score(1, 2));
+    }
+
+    private static List<Match> prepareWeek1() {
+        Match matchToDo1 = new Match(myth, snsh);
+        Match matchToDo2 = new Match(ouat, mcon);
+        Match matchToDo3 = new Match(aoma, jmgg);
+        Match matchToDo4 = new Match(znt, ara);
+
+        return List.of(matchToDo1, matchToDo2, matchToDo3, matchToDo4);
     }
 
     private static void playWeek1(Championship championship) {
@@ -77,16 +164,6 @@ public class Main {
     }
 
     private static Championship getBeneluxChampionship() {
-        String znt = "ZennIt";
-        String mcon = "mCon esports";
-        String snsh = "Senshi eports";
-        String aoma = "A One Man Army";
-        String myth = "Myth Esport";
-        String ouat = "Once Upon A Team";
-        String jmgg = "Jörmungang";
-        String ara = "Aurora";
-
-
         Team zntTeam = new Team(znt);
         Team mconTeam = new Team(mcon);
         Team snshTeam = new Team(snsh);
@@ -98,54 +175,22 @@ public class Main {
 
         List<Team> teams = List.of(zntTeam, mconTeam, snshTeam, aomaTeam, mythTeam, ouatTeam, jmggTeam, araTeam);
 
-        Match matchToDoW1_1 = new Match(myth, snsh);
-        Match matchToDoW1_2 = new Match(ouat, mcon);
-        Match matchToDoW1_3 = new Match(aoma, jmgg);
-        Match matchToDoW1_4 = new Match(znt, ara);
+        List<Match> week1 = prepareWeek1();
+        List<Match> week2 = prepareWeek2();
+        List<Match> week3 = prepareWeek3();
+        List<Match> week4 = prepareWeek4();
+        List<Match> week5 = prepareWeek5();
+        List<Match> week6 = prepareWeek6();
+        List<Match> week7 = prepareWeek7();
 
-        Match matchToDoW2_1 = new Match(znt, mcon);
-        Match matchToDoW2_2 = new Match(aoma,snsh);
-        Match matchToDoW2_3 = new Match(ouat, ara);
-        Match matchToDoW2_4 = new Match(myth, jmgg);
-
-        List<Match> matchList1 = List.of(matchToDoW1_1, matchToDoW1_2, matchToDoW1_3, matchToDoW1_4, matchToDoW2_1, matchToDoW2_2, matchToDoW2_3, matchToDoW2_4);
-
-        Match matchToDoW3_1 = new Match(myth, ara);
-        Match matchToDoW3_2 = new Match(ouat, jmgg);
-        Match matchToDoW3_3 = new Match(aoma, mcon);
-        Match matchToDoW3_4 = new Match(znt, snsh);
-
-        Match matchToDoW4_1 = new Match(znt, jmgg);
-        Match matchToDoW4_2 = new Match(myth, mcon);
-        Match matchToDoW4_3 = new Match(ouat, snsh);
-        Match matchToDoW4_4 = new Match(aoma, ara);
-
-        List<Match> matchList2 = List.of(matchToDoW3_1, matchToDoW3_2, matchToDoW3_3, matchToDoW3_4, matchToDoW4_1, matchToDoW4_2, matchToDoW4_3, matchToDoW4_4);
-
-        Match matchToDoW5_1 = new Match(mcon, jmgg);
-        Match matchToDoW5_2 = new Match(snsh, ara);
-        Match matchToDoW5_3 = new Match(znt, aoma);
-        Match matchToDoW5_4 = new Match(ouat, myth);
-
-        List<Match> matchList3 = List.of(matchToDoW5_1, matchToDoW5_2, matchToDoW5_3, matchToDoW5_4);
-
-        Match matchToDoW6_1 = new Match(mcon, ara);
-        Match matchToDoW6_2 = new Match(znt, ouat);
-        Match matchToDoW6_3 = new Match(snsh, jmgg);
-        Match matchToDoW6_4 = new Match(aoma, myth);
-
-        Match matchToDoW7_1 = new Match(ouat, aoma);
-        Match matchToDoW7_2 = new Match(ara, jmgg);
-        Match matchToDoW7_3 = new Match(znt, myth);
-        Match matchToDoW7_4 = new Match(mcon, snsh);
-
-
-        List<Match> matchList4 = List.of(matchToDoW6_1, matchToDoW6_2, matchToDoW6_3, matchToDoW6_4, matchToDoW7_1, matchToDoW7_2, matchToDoW7_3, matchToDoW7_4);
-
-        ArrayList<Match> matchList = new ArrayList<>(matchList1);
-        matchList.addAll(matchList2);
-        matchList.addAll(matchList3);
-        matchList.addAll(matchList4);
+        ArrayList<Match> matchList = new ArrayList<>();
+        matchList.addAll(week1);
+        matchList.addAll(week2);
+        matchList.addAll(week3);
+        matchList.addAll(week4);
+        matchList.addAll(week5);
+        matchList.addAll(week6);
+        matchList.addAll(week7);
 
         return new Championship(teams, matchList);
     }
